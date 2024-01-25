af582dd0f9ed2fa95b6f6b86c19e6c77bb2f519d
Sten Røkke <sten.ivar.rokke@nav.no>
2024-01-25 20:28:21 +0100

    fix: clean up variables and add comment

--- internal/opensearch/opensearch.go
+++ internal/opensearch/opensearch.go
@@ -14,22 +14,23 @@ import (
 
 func Handler(ctx context.Context, client *opensearch.Client) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, _ *http.Request) {
+		// Creating document
 		indexName := "contests"
 		epoch := fmt.Sprintf("%d", time.Now().UnixNano())
-		document := strings.NewReader(`{ "Application": "contests" }`)
 		indexRequest := opensearchapi.IndexRequest{
 			Index:      indexName,
 			DocumentID: epoch,
-			Body:       document,
+			Body:       strings.NewReader(`{ "Application": "contests" }`),
 		}
 
-		rs, err := indexRequest.Do(ctx, client)
+		res, err := indexRequest.Do(ctx, client)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("create document: %v", err), http.StatusInternalServerError)
 			return
 		}
-		defer rs.Body.Close()
 		log.Info("Successfully created document in opensearch")
+
+		defer res.Body.Close()
 
 		// Retrieving same document
 		getRequest := opensearchapi.GetRequest{
@@ -37,7 +38,7 @@ func Handler(ctx context.Context, client *opensearch.Client) func(http.ResponseW
 			DocumentID: epoch,
 		}
 
-		rs, err = getRequest.Do(ctx, client)
+		_, err = getRequest.Do(ctx, client)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("get document: %v", err), http.StatusInternalServerError)
 			return
