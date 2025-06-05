cee6c6d0dd65cf4b493db374276fa82fb16dfbaf
Morten Lied Johansen <morten.lied.johansen@nav.no>
2025-06-05 15:38:09 +0200

    Add more logging around database

--- internal/database/database.go
+++ internal/database/database.go
@@ -13,6 +13,7 @@ func Handler(url string, logger log.FieldLogger) func(http.ResponseWriter, *http
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
+		logger.Info("Opening connection to database")
 		db, err := sql.Open("postgres", url)
 		if err != nil {
 			logger.Errorf("Opening connection to database: %s", err)
@@ -20,6 +21,7 @@ func Handler(url string, logger log.FieldLogger) func(http.ResponseWriter, *http
 			return
 		}
 		defer db.Close()
+		logger.Info("Pinging database")
 		err = db.PingContext(ctx)
 		if err != nil {
 			logger.Errorf("Pinging database: %s", err)
